5d07121df19ce8f0204f25132287abdf997fcf9f
Ferch42 <fernando.hsieh@usp.br>
2016-07-02 16:51:14 -0300

    bug fixes

--- binario.java
+++ binario.java
@@ -5,10 +5,10 @@ class binario{
 		Scanner stdin=new Scanner(System.in);
 		System.out.println("Digite o numero binario");
 		binario1= stdin.nextInt();
-		double resultado,b=0;
-		double binario2;
+		double resultado=0,b=0;
+		double binario2=0;
 		binario2=binario1;
-		while(binario2%10=!0){
+		while(binario2%10!=0){
 			resultado= resultado + (binario1%10)*Math.pow(2,b);
 			binario1=binario1/10;
 			binario2=binario1;
