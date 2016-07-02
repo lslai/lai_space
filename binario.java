718905c2fc3ee4bb705289e7ed4f95e5ea304c3a
Ferch42 <fernando.hsieh@usp.br>
2016-07-02 16:37:11 -0300

    double

--- binario.java
+++ binario.java
@@ -1,13 +1,14 @@
 import java.util.*;
 class binario{
 	public static void main(String[]args){
-		int binario;
+		double binario;
 		Scanner stdin=new Scanner(System.in);
 		System.out.println("Digite o numero binario");
 		binario= stdin.nextInt();
-		int resultado,b=0;
+		double resultado,b=0;
 		while(binario%10=!0){
-			resultado= resultado + binario%10*Math.pow(2,b);
+			resultado= resultado + (binario%10)*Math.pow(2,b);
+			binario=binario/10;
 		}
 		System.out.println(resultado);
 
