49a970bbd224a6455a898df239e15bd15ccda476
Ferch42 <fernando.hsieh@usp.br>
2016-07-02 16:45:05 -0300

    bug fixes

--- binario.java
+++ binario.java
@@ -1,14 +1,17 @@
 import java.util.*;
 class binario{
 	public static void main(String[]args){
-		double binario;
+		double binario1;
 		Scanner stdin=new Scanner(System.in);
 		System.out.println("Digite o numero binario");
-		binario= stdin.nextInt();
+		binario1= stdin.nextInt();
 		double resultado,b=0;
-		while(binario%10=!0){
-			resultado= resultado + (binario%10)*Math.pow(2,b);
-			binario=binario/10;
+		double binario2;
+		binario2=binario1;
+		while(binario2%10=!0){
+			resultado= resultado + (binario1%10)*Math.pow(2,b);
+			binario1=binario1/10;
+			binario2=binario1;
 		}
 		System.out.println(resultado);
 
